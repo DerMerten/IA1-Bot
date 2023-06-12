b1634f50529f583661367de2bc5af490cafa17a6
DerMerten <90412698+DerMerten@users.noreply.github.com>
2023-06-12 15:13:49 +0000

    hey

---
+++ config/config.py
@@ -0,0 +1,31 @@
+status = ["Unterricht beginnt um 07:45 Uhr",
+          "Scheiß Kinder"]
+
+# the message displayed when bother is triggered, set it to whatever, I make it ping me with this message
+no = "<@421392717491339265> sum dud wants something from u, idk :/"
+
+
+# enter the nicknames that triggers the no message above
+bother = ["DerMerten"]
+
+# makes howgay command always return 0% for this id
+ungayid = 669159345061363712
+
+#NICHT LÖSCHEN!!!
+supergayid = 866782637849706516
+
+member_role = "IA1"
+
+ServerID = 1018983239920144466
+reactionroles = {
+    "1019997350795694102":{
+        "🅰️": {
+            "roles": [1020002833778606240]
+        },
+        "🅱️": {
+            "roles": [1020012980479475783]
+        }
+    }
+}
+
+beleigiungen = ["Unterricht fängt um 07:45.", "Gib dein Zeugnis wieder bei deiner alten schule ab."]
